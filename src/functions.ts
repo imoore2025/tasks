9268356569b35d813dacd5eaba257d82eb1a2957
Isaiah Moore <imoore@udel.edu>
2026-09-02 12:05:20 -0400

    functions.ts | solved add3 function and shout function.

--- src/functions.ts
+++ src/functions.ts
@@ -12,7 +12,11 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    return (
+        (first > 0 ? first : 0) +
+        (second > 0 ? second : 0) +
+        (third > 0 ? third : 0)
+    );
 }
 
 /**
@@ -20,7 +24,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    return message.toUpperCase();
 }
 
 /**
